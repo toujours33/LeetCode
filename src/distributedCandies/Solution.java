9c0f5f9e2b9c7acc25bbe3238dc59c56c33e54c1
toujours33 <1012242313@qq.com>
2018-03-15 09:09:15 +0800

    蒸熟数组取不同数最多的一组 #575

--- src/distributedCandies/Solution.java
+++ src/distributedCandies/Solution.java
@@ -1,9 +1,25 @@
 package distributedCandies;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
 	
-	public static int distributedCandies() {
-		
+	public static int distributedCandies(int[] candies) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < candies.length; i++) {
+			map.put(i, i);
+		}
+		return map.size()>candies.length/2 ? candies.length/2 : map.size();
+	}
+	
+	public static int distributedCandies2(int[] candies) {
+		Set<Integer> set = new HashSet<>();
+		for (Integer integer : candies) {
+			set.add(integer);
+		}
+		return set.size()>candies.length/2?candies.length/2:set.size();
 	}
 
 }
